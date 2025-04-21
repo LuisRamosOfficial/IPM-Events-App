5218bd58c0f2d05b3cdf7114d5a22f0da5735ee2
Luis <fc62719@alunos.fc.ul.pt>
2025-04-21 23:12:59 +0100

    Express Initialization

--- index.js
+++ index.js
@@ -1,12 +1,49 @@
 import express from 'express';
+import path from 'path';
 
 const app = express()
 app.use(express.static('public'))
 const port = 3000
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+
+
 
 app.get('/', (req, res) => {
-  res.sendFile('./index.html')
+  res.sendFile('./index.html', {root: path.resolve()})
 })
+
+
+app.get('/login', (req, res) => {
+  res.sendFile('./login.html', {root: path.resolve()})
+})
+
+
+
+app.get('/register', (req, res) => {
+
+
+
+    res.sendFile('./register.html', {root: path.resolve()})
+})
+app.post('/registering', (req, res) => {
+
+    console.log(req.body);
+
+
+    res.redirect(307, "/register");
+})
+
+
+
+
+app.get('/home', (req, res) => {
+  res.sendFile('./home.html', {root: path.resolve()})
+})
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
